70ad78f040a7f671537e6cb422a547058fb19974
= <daniloagostinhosilva@gmail.com>
2023-07-27 14:35:02 -0300

    Continuar

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
 
 const app = express();
 
 // Conectando ao MongoDB
-mongoose.connect('mongodb+srv://danilodev:091011@cluster0.w65a0yv.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.w65a0yv.mongodb.net/`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado ao MongoDB com sucesso!'));
 
 app.use(express.json());
