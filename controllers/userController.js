eeda104e98b9c2c9da563bac7da20b5ba4e52fcc
= <daniloagostinhosilva@gmail.com>
2023-07-28 22:37:12 -0300

    Refatorando controller de funções para classe

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,102 +11,103 @@ const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
 const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 const JWT_SECRET = process.env.JWT_SECRET;
 
-console.log(EMAIL_USERNAME, EMAIL_PASSWORD)
-
-exports.register = async (req, res) => {
-    try {
-        let existingUser = await User.findOne({ email: req.body.email });
-        if (existingUser) {
-            return res.status(400).json({ error: 'Email já cadastrado na base!' });
+class UserController {
+    static async register(req, res) {
+        try {
+            let existingUser = await User.findOne({ email: req.body.email });
+            if (existingUser) {
+                return res.status(400).json({ error: 'Email já cadastrado na base!' });
+            }
+            let user = await User.create(req.body);
+            let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            res.json({ token });
+        } catch (err) {
+            res.status(400).json({ error: err.message });
         }
-        let user = await User.create(req.body);
-        let token = jwt.sign({ id: user._id }, JWT_SECRET);
-        res.json({ token });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
+    }
+
+    static async login(req, res) {
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                res.json({ mensagem: 'Usuário não encontrado' });
+                throw new Error('Usuário não encontrado');
+            }
+            let match = await bcrypt.compare(req.body.password, user.password);
+            if (!match) {
+                return res.json({ mensagem: 'Senha inválida' });
+            }
+            let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            res.json({ token });
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    }
+
+    static async recover(req, res) {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(400).json({ messagem: 'Email não encontrado' });
+            }
+
+            // Gere um token de reset de senha
+            user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hora
+            await user.save();
+
+            // Enviar email de recuperação de senha
+            const transporter = nodemailer.createTransport({
+                service: 'Outlook365',
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
+
+            const mailOptions = {
+                to: user.email,
+                from: process.env.EMAIL_USERNAME,
+                subject: 'Recuperação de senha',
+                text: `Clique no link a seguir ou cole-o em seu navegador para concluir o processo: \n\nhttp://${req.headers.host}/user/reset\n\n Se você não solicitou isso, ignore este e-mail e sua senha permanecerá inalterada. \n`
+            };
+
+            transporter.sendMail(mailOptions, (err) => {
+                if (err) {
+                  console.log(err); // Adicione essa linha
+                  return res.status(500).json({ error: 'Não foi possível enviar o e-mail de recuperação!' });
+                }
+                res.json({ message: 'E-mail de recuperação enviado', resetPassWordToken: user.resetPasswordToken });
+              });
+
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
+
+    static async resetPassword(req, res) {
+        try {
+            const user = await User.findOne({
+                resetPasswordToken: req.body.token,
+                resetPasswordExpires: { $gt: Date.now() }
+            });
+
+            if (!user) {
+                return res.status(400).json({ error: 'Token de reset da senha inválido ou expirado!' });
+            }
+
+            // Set the new password
+            user.password = req.body.password;
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+
+            await user.save();
+
+            res.json({ message: 'Senha resetada com sucesso!' });
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
     }
 }
 
-exports.login = async (req, res) => {
-    try {
-        let user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            res.json({ mensagem: 'Usuário não encontrado' });
-            throw new Error('Usuário não encontrado');
-        }
-        let match = await bcrypt.compare(req.body.password, user.password);
-        if (!match) {
-            return res.json({ mensagem: 'Senha inválida' });
-        }
-        let token = jwt.sign({ id: user._id }, JWT_SECRET);
-        res.json({ token });
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-}
-
-exports.recover = async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            return res.status(400).json({ messagem: 'Email não encontrado' });
-        }
-
-        // Gere um token de reset de senha
-        user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hora
-        await user.save();
-
-        // Enviar email de recuperação de senha
-        const transporter = nodemailer.createTransport({
-            service: 'Outlook365',
-            auth: {
-                user: EMAIL_USERNAME,
-                pass: EMAIL_PASSWORD
-            }
-        });
-
-        const mailOptions = {
-            to: user.email,
-            from: process.env.EMAIL_USERNAME,
-            subject: 'Recuperação de senha',
-            text: `Clique no link a seguir ou cole-o em seu navegador para concluir o processo: \n\nhttp://${req.headers.host}/user/reset\n\n Se você não solicitou isso, ignore este e-mail e sua senha permanecerá inalterada. \n`
-        };
-
-        transporter.sendMail(mailOptions, (err) => {
-            if (err) {
-              console.log(err); // Adicione essa linha
-              return res.status(500).json({ error: 'Não foi possível enviar o e-mail de recuperação!' });
-            }
-            res.json({ message: 'E-mail de recuperação enviado', resetPassWordToken: user.resetPasswordToken });
-          });
-          
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-};
-
-exports.resetPassword = async (req, res) => {
-    try {
-      const user = await User.findOne({
-        resetPasswordToken: req.body.token,
-        resetPasswordExpires: { $gt: Date.now() }
-      });
-  
-      if (!user) {
-        return res.status(400).json({ error: 'Token de reset da senha inválido ou expirado!' });
-      }
-  
-      // Set the new password
-      user.password = req.body.password;
-      user.resetPasswordToken = undefined;
-      user.resetPasswordExpires = undefined;
-  
-      await user.save();
-  
-      res.json({ message: 'Senha resetada com sucesso!' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-};
-  
+module.exports = UserController;
